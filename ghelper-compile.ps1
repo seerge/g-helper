04a7d845af621a2618a5f89901825ec0d7210979
Serge <serge@asus>
2023-02-12 21:12:02 +0100

    Performance mode switching

---
+++ ghelper-compile.ps1
@@ -0,0 +1,2 @@
+ps2exe .\gpu.ps1 g14-helper.exe -title 'G14 Helper' -DPIAware -winFormsDPIAware -requireAdmin -iconFile 'standard.ico' -noConsole -copyright 'G14 Helper Tool' -noOutput
+.\g14-helper.exe
--- gpu-compile.ps1
+++
@@ -1,2 +0,0 @@
-ps2exe .\gpu.ps1 g14-gpu.exe -title 'G14 GPU Mode' -DPIAware -winFormsDPIAware -requireAdmin -iconFile 'standard.ico' -noConsole -copyright 'G14 GPU Mode'
-.\g14-gpu.exe
