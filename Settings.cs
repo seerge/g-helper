77a7a5b1bb1875932b8fb776a3eb4c60b3851d18
seerge <serge.samusya@gmail.com>
2023-02-20 16:59:44 +0100

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -399,13 +399,9 @@ namespace GHelper
         public int InitGPUMode()
         {
 
-            // Check if laptop is plugged in at boot. If true, set gpu mode accordingly
-            bool isPlugged = (SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online);
-            AutoGPUMode(isPlugged ? 1 : 0);
 
             int eco = Program.wmi.DeviceGet(ASUSWmi.GPUEco);
             int mux = Program.wmi.DeviceGet(ASUSWmi.GPUMux);
-
 
             int GpuMode;
 
