e7a4537098873c3509ac364b5ee704d4e44e5052
Serge <5920850+seerge@users.noreply.github.com>
2023-07-19 16:17:05 +0200

    Cleanup

--- app/Helpers/ProcessHelper.cs
+++ app/Helpers/ProcessHelper.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GHelper.Helpers
 {
