ab2dd4d685a48c3a2d19f980e5abfa21d10ae667
IceStormNG <carstenbraun@live.de>
2023-07-23 11:54:54 +0200

    Added mouse icon

--- app/Properties/Resources.Designer.cs
+++ app/Properties/Resources.Designer.cs
@@ -323,6 +323,26 @@ namespace GHelper.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_maus_32 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_maus_32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_maus_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_maus_48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_microphone_96 {
             get {
                 object obj = ResourceManager.GetObject("icons8_microphone_96", resourceCulture);
