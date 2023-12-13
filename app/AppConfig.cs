d6fe7f41c1ab5a1633e5f2b04413e96ef90204fd
Serge <5920850+seerge@users.noreply.github.com>
2023-12-13 23:06:28 +0100

    Remove unsupported RGB colors from GA502IU https://github.com/seerge/g-helper/issues/1740

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -345,7 +345,7 @@ public static class AppConfig
 
     public static bool IsSingleColor()
     {
-        return  ContainsModel("GA401") || ContainsModel("FX517Z") || ContainsModel("FX516P") || ContainsModel("X13") || IsARCNM();
+        return  ContainsModel("GA401") || ContainsModel("FX517Z") || ContainsModel("FX516P") || ContainsModel("X13") || IsARCNM() || ContainsModel("GA502IU");
     }
 
     public static bool IsStrix()
