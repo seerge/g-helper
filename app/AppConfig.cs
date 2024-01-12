c0f88191df9300f79ce19e3baea7e36e48529d1d
Serge <5920850+seerge@users.noreply.github.com>
2024-01-12 21:32:10 +0100

    Per key RGB option for G513QM https://github.com/seerge/g-helper/discussions/1906

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -384,7 +384,7 @@ public static class AppConfig
 
     public static bool IsStrixLimitedRGB()
     {
-        return ContainsModel("G614JV") || ContainsModel("G614JZ") || ContainsModel("G512LI") || ContainsModel("G513R") || ContainsModel("G713PV") || ContainsModel("G513IE") || ContainsModel("G713RC") || ContainsModel("G513QM");
+        return (ContainsModel("G614JV") || ContainsModel("G614JZ") || ContainsModel("G512LI") || ContainsModel("G513R") || ContainsModel("G713PV") || ContainsModel("G513IE") || ContainsModel("G713RC") || ContainsModel("G513QM")) && !Is("per_key_rgb");
     }
 
     public static bool IsNoDirectRGB()
