e57f61b07aa5bae5de4006b4cdfcab998c8039cd
Serge <5920850+seerge@users.noreply.github.com>
2024-01-30 14:35:03 +0100

    Release Update Logic

--- app/Ryzen/RyzenControl.cs
+++ app/Ryzen/RyzenControl.cs
@@ -154,8 +154,9 @@ namespace Ryzen
 
         public static void DownloadRing()
         {
-            var appVersion = new Version(Assembly.GetExecutingAssembly().GetName().Version.ToString());
-            string requestUri = "https://github.com/seerge/g-helper/releases/download/v" + appVersion.Major + "." + appVersion.Minor + "/PluginAdvancedSettings.zip";
+            //var appVersion = new Version(Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            //string requestUri = "https://github.com/seerge/g-helper/releases/download/v" + appVersion.Major + "." + appVersion.Minor + "/PluginAdvancedSettings.zip";
+            string requestUri = "https://github.com/seerge/g-helper/releases/download/v0.150/PluginAdvancedSettings.zip";
 
             Uri uri = new Uri(requestUri);
 
