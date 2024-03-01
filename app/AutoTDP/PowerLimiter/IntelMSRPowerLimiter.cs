4af2bf63c7f5e6674b463e388c4efe250e10d284
IceStormNG <carstenbraun@live.de>
2024-03-01 07:49:53 +0100

    Remove unnecessary usings

--- app/AutoTDP/PowerLimiter/IntelMSRPowerLimiter.cs
+++ app/AutoTDP/PowerLimiter/IntelMSRPowerLimiter.cs
@@ -1,10 +1,4 @@
-﻿using HidSharp.Reports.Units;
-using Ryzen;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ryzen;
 
 namespace GHelper.AutoTDP.PowerLimiter
 {
