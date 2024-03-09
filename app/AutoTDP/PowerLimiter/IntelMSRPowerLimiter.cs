937d3293a4fa17b59b39c3a5825e09b11df72b67
IceStormNG <carstenbraun@live.de>
2024-03-09 14:35:04 +0100

    Extracted Intel CPU controls. This way it can be used for other means as well.

--- app/AutoTDP/PowerLimiter/IntelMSRPowerLimiter.cs
+++ app/AutoTDP/PowerLimiter/IntelMSRPowerLimiter.cs
@@ -1,4 +1,5 @@
 ﻿using GHelper.Helpers;
+using GHelper.Intel;
 using Ryzen;
 
 namespace GHelper.AutoTDP.PowerLimiter
@@ -6,30 +7,13 @@ namespace GHelper.AutoTDP.PowerLimiter
 
     internal class IntelMSRPowerLimiter : IPowerLimiter
     {
-        public static readonly uint MSR_PKG_POWER_LIMIT = 0x610;
-        public static readonly uint MSR_RAPL_POWER_UNIT = 0x606;
 
-        private Ols ols;
-
-        private static bool DRIVER_LOADED = false;
-
-        private uint DefaultEax = 0; // Set on first reading
-        private uint DefaultEdx = 0;
-
-        //Lower 14 bits are the power limits
-        private uint PL1_MASK = 0x3FFF;
-        private uint PL2_MASK = 0x3FFF;
-
-        //The power unit factor (Default is 0.125 for most Intel CPUs).
-        private double PowerUnit = 0x0;
+        uint backupPl1 = 0;
+        uint backupPl2 = 0;
 
         public IntelMSRPowerLimiter()
         {
-            ols = new Ols();
-            ols.InitializeOls();
-            DRIVER_LOADED = true;
-            ReadPowerUnit();
-            Logger.WriteLine("[AutoTDPService] Read MSR_RAPL_POWER_UNIT: " + PowerUnit);
+            IntelCoreControl.LogCurrentState();
         }
 
         public int GetMinInterval()
@@ -44,19 +28,9 @@ namespace GHelper.AutoTDP.PowerLimiter
 
         public static bool IsAvailable()
         {
-            if (!ProcessHelper.IsUserAdministrator())
+            if (!ProcessHelper.IsUserAdministrator() || !IntelCoreControl.IsIntel())
             {
                 return false;
-            }
-
-            if (RyzenControl.IsAMD())
-            {
-                return false;
-            }
-
-            if (DRIVER_LOADED)
-            {
-                return true;
             }
 
             Ols o = new Ols();
@@ -72,136 +46,41 @@ namespace GHelper.AutoTDP.PowerLimiter
                 return true;
             }
 
-            LogOLSState(err);
-
             return false;
-        }
-
-        private static void LogOLSState(uint err)
-        {
-            switch (err)
-            {
-                case (uint)Ols.OlsDllStatus.OLS_DLL_NO_ERROR:
-                    return;
-                case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_NOT_LOADED:
-                    Logger.WriteLine("[AutoTDPService] Intel MSR Error: OLS_DRIVER_NOT_LOADED");
-                    break;
-                case (uint)Ols.OlsDllStatus.OLS_DLL_UNSUPPORTED_PLATFORM:
-                    Logger.WriteLine("[AutoTDPService] Intel MSR Error: OLS_DLL_UNSUPPORTED_PLATFORM");
-                    break;
-                case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_NOT_FOUND:
-                    Logger.WriteLine("[AutoTDPService] Intel MSR Error: OLS_DLL_DRIVER_NOT_FOUND");
-                    break;
-                case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_UNLOADED:
-                    Logger.WriteLine("[AutoTDPService] Intel MSR Error: OLS_DLL_DRIVER_UNLOADED");
-                    break;
-                case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_NOT_LOADED_ON_NETWORK:
-                    Logger.WriteLine("[AutoTDPService] Intel MSR Error: OLS_DLL_DRIVER_NOT_LOADED_ON_NETWORK");
-                    break;
-                case (uint)Ols.OlsDllStatus.OLS_DLL_UNKNOWN_ERROR:
-                    Logger.WriteLine("[AutoTDPService] Intel MSR Error: OLS_DLL_UNKNOWN_ERROR");
-                    break;
-            }
         }
 
         public void SavePowerLimits()
         {
-            DefaultEax = 0;
-            DefaultEdx = 0;
-
-            if (ols.Rdmsr(MSR_PKG_POWER_LIMIT, ref DefaultEax, ref DefaultEdx) == 0)
-            {
-                Logger.WriteLine("[AutoTDPService] Failed to Read MSR_PKG_POWER_LIMIT");
-                LogOLSState(ols.GetDllStatus());
-            }
+            backupPl1 = IntelCoreControl.GetPL1();
+            backupPl2 = IntelCoreControl.GetPL2();
         }
 
-        public void ReadPowerUnit()
-        {
-            uint eax = 0;
-            uint edx = 0;
-
-            if (ols.Rdmsr(MSR_RAPL_POWER_UNIT, ref eax, ref edx) == 0)
-            {
-                Logger.WriteLine("[AutoTDPService] Failed to Read MSR_RAPL_POWER_UNIT");
-                LogOLSState(ols.GetDllStatus());
-            }
-
-
-            uint pwr = eax & 0x03;
-
-            PowerUnit = 1 / Math.Pow(2, pwr);
-        }
 
         public void SetCPUPowerLimit(double watts)
         {
-            uint eax = 0;
-            uint edx = 0;
-
-
-            if (ols.Rdmsr(MSR_PKG_POWER_LIMIT, ref eax, ref edx) == 0)
-            {
-                Logger.WriteLine("[AutoTDPService] Failed to Read MSR_PKG_POWER_LIMIT");
-                LogOLSState(ols.GetDllStatus());
-            }
-
-            //Power limit in RAPL units
-            uint wattsRapl = (uint)(watts / PowerUnit);
-
-            //Set limits for both PL1 and PL2
-            uint eaxFilterd = eax & ~PL1_MASK;
-            uint edxFilterd = edx & ~PL2_MASK;
-
-            eaxFilterd |= wattsRapl;
-            edxFilterd |= wattsRapl;
-
-            //Enable clamping
-            eaxFilterd |= 0x8000;
-            edxFilterd |= 0x8000;
-
-            if (ols.Wrmsr(0x610, eaxFilterd, edxFilterd) == 0)
-            {
-                Logger.WriteLine("[AutoTDPService] Failed to Write MSR_PKG_POWER_LIMIT");
-                LogOLSState(ols.GetDllStatus());
-            }
+            IntelCoreControl.SetPowerLimits((uint)Math.Ceiling(watts), (uint)Math.Ceiling(watts), clampPl1: true, clampPl2: true);
         }
 
 
         public int GetCPUPowerLimit()
         {
-            uint eax = 0;
-            uint edx = 0;
+            Logger.WriteLine("[AutoTDPService] Read Power Limit - PL1: " + IntelCoreControl.GetPL1() + "W, PL2: " + IntelCoreControl.GetPL2() + "W");
 
-            if (ols.Rdmsr(MSR_PKG_POWER_LIMIT, ref eax, ref edx) == 0)
-            {
-                Logger.WriteLine("[AutoTDPService] Failed to Read MSR_PKG_POWER_LIMIT");
-                LogOLSState(ols.GetDllStatus());
-            }
-
-            uint pl1 = eax & PL1_MASK;
-            uint pl2 = edx & PL2_MASK;
-
-
-            Logger.WriteLine("[AutoTDPService] Read Power Limit - PL1: " + pl1 * PowerUnit + "W, PL2: " + pl2 * PowerUnit + "W");
-
-            return (int)(pl1 * PowerUnit);
+            return (int)IntelCoreControl.GetPL1();
         }
 
 
         public void ResetPowerLimits()
         {
-            if (DefaultEax == 0)
+            if (backupPl1 == 0)
             {
                 return;
             }
-            ols.Wrmsr(MSR_PKG_POWER_LIMIT, DefaultEax, DefaultEdx);
+            IntelCoreControl.SetPowerLimits(backupPl1, backupPl2);
         }
 
         public void Dispose()
         {
-            ols.DeinitializeOls();
-            ols.Dispose();
-            DRIVER_LOADED = true;
         }
     }
 }
