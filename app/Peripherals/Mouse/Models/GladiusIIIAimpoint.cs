574c6ccda12ddcc4607c6a69c5438e5c1c8a31e9
IceStormNG <carstenbraun@live.de>
2024-03-22 17:29:47 +0100

    Fix for Gladius III Aimpoint and Gladius III Aimpoint EVA02 (#2266)
    * Support for Strix Carry (P508)
    
    * Fixes polling rate, angle snapping and debounce for Gladius II Origin.
    
    * The Gen2 version of the TuF M3 uses 0-100 for brightness.
    
    * Adds support for ROG Strix Impact III (P518)
    
    * Import/Export feature for mice.
    
    * Suppor for Strix Impact (P303)
    
    * Support for Strix Impact II Electro Punk
    
    * Strix Carry has 50 DPI minimum and increments of 50.
    
    * Respect top-most setting of GHelper
    
    * Fixes to the buttons to be wider to fit longer translations.
    
    * Basic support for the Galdius III EVA02
    
    * Gladius wireless and wired PIDs were switched

--- app/Peripherals/Mouse/Models/GladiusIIIAimpoint.cs
+++ app/Peripherals/Mouse/Models/GladiusIIIAimpoint.cs
@@ -3,11 +3,11 @@
     //P711
     public class GladiusIIIAimpoint : AsusMouse
     {
-        public GladiusIIIAimpoint() : base(0x0B05, 0x1A70, "mi_00", true)
+        public GladiusIIIAimpoint() : base(0x0B05, 0x1A72, "mi_00", true)
         {
         }
 
-        protected GladiusIIIAimpoint(ushort vendorId, bool wireless) : base(0x0B05, vendorId, "mi_00", wireless)
+        protected GladiusIIIAimpoint(ushort productId, bool wireless) : base(0x0B05, productId, "mi_00", wireless)
         {
         }
 
@@ -94,13 +94,56 @@
 
     public class GladiusIIIAimpointWired : GladiusIIIAimpoint
     {
-        public GladiusIIIAimpointWired() : base(0x1A72, false)
+        public GladiusIIIAimpointWired() : base(0x1A70, false)
         {
         }
 
         public override string GetDisplayName()
         {
             return "ROG Gladius III Aimpoint (Wired)";
+        }
+    }
+
+    public class GladiusIIIAimpointEva2 : GladiusIIIAimpoint
+    {
+        public GladiusIIIAimpointEva2() : base(0x1B0C, true)
+        {
+        }
+
+        public GladiusIIIAimpointEva2(ushort productId) : base(productId, false)
+        {
+        }
+
+        public override string GetDisplayName()
+        {
+            return "ROG Gladius III Eva 2 (Wireless)";
+        }
+
+        public override LightingZone[] SupportedLightingZones()
+        {
+            return new LightingZone[] { LightingZone.Logo };
+        }
+
+        public override bool IsLightingModeSupported(LightingMode lightingMode)
+        {
+            return lightingMode == LightingMode.Static
+                || lightingMode == LightingMode.Breathing
+                || lightingMode == LightingMode.ColorCycle
+                || lightingMode == LightingMode.React
+                || lightingMode == LightingMode.Comet
+                || lightingMode == LightingMode.BatteryState;
+        }
+    }
+
+    public class GladiusIIIAimpointEva2Wired : GladiusIIIAimpointEva2
+    {
+        public GladiusIIIAimpointEva2Wired() : base(0x1B0A)
+        {
+        }
+
+        public override string GetDisplayName()
+        {
+            return "ROG Gladius III Eva 2 (Wired)";
         }
     }
 }
