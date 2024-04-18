ae41aedb1ec8c397ae61df9dfc44fb511651afa9
Serge <5920850+seerge@users.noreply.github.com>
2024-04-18 18:03:56 +0200

    Support for inverted fn-lock https://github.com/seerge/g-helper/issues/2445

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -404,6 +404,11 @@ public static class AppConfig
         return ContainsModel("GA503") || IsSlash();
     }
 
+    public static bool IsInvertedFNLock()
+    {
+        return ContainsModel("M140");
+    }
+
     public static bool IsOLED()
     {
         return ContainsModel("OLED") || IsSlash() || ContainsModel("M7600") || ContainsModel("UX64") || ContainsModel("UX34") || ContainsModel("UX53") || ContainsModel("K360") || ContainsModel("X150") || ContainsModel("M350") || ContainsModel("K650") || ContainsModel("UM53") || ContainsModel("K660") || ContainsModel("UX84") || ContainsModel("M650") || ContainsModel("K340") || ContainsModel("K350") || ContainsModel("M140");
