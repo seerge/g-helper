34ec26dba0568319f8b6a9ea1ddb7e5922eaa860
Serge <5920850+seerge@users.noreply.github.com>
2024-06-01 22:33:16 +0200

    Charge limits for GA503R

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -633,7 +633,7 @@ public static class AppConfig
 
     public static bool IsChargeLimit6080()
     {
-        return ContainsModel("GU605") || (IsTUF() && !(ContainsModel("FX507Z") || ContainsModel("FA617")));
+        return ContainsModel("GU605") || ContainsModel("GA503R") || (IsTUF() && !(ContainsModel("FX507Z") || ContainsModel("FA617")));
 
     }
 
