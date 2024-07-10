2ed6f1fefb494a429987e54e759460d0a4bad0b4
Serge <5920850+seerge@users.noreply.github.com>
2024-07-10 17:50:38 +0200

    Ultimate mode tweak for GU603VV https://github.com/seerge/g-helper/issues/2825

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -527,7 +527,7 @@ public static class AppConfig
 
     public static bool NoAutoUltimate()
     {
-        return ContainsModel("G614") || ContainsModel("GU604") || ContainsModel("FX507") || ContainsModel("G513") || ContainsModel("FA617") || ContainsModel("G834") || ContainsModel("GA403") || ContainsModel("GU605");
+        return ContainsModel("G614") || ContainsModel("GU604") || ContainsModel("FX507") || ContainsModel("G513") || ContainsModel("FA617") || ContainsModel("G834") || ContainsModel("GA403") || ContainsModel("GU605") || ContainsModel("GU603VV");
     }
 
 
