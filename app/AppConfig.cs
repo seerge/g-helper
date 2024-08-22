cb3bf3ea90ad6a3b5f4629a6a57bad54320145be
Serge <5920850+seerge@users.noreply.github.com>
2024-08-22 11:30:38 +0200

    Swap brightness hotkeys for FA706IH https://github.com/seerge/g-helper/issues/3009

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -394,7 +394,7 @@ public static class AppConfig
     // Devices with bugged bios command to change brightness
     public static bool SwappedBrightness()
     {
-        return ContainsModel("FA506IH") || ContainsModel("FA506IC") || ContainsModel("FX506LU") || ContainsModel("FX506IC") || ContainsModel("FX506LH") || ContainsModel("FA506IV") || ContainsModel("FA706IC");
+        return ContainsModel("FA506IH") || ContainsModel("FA506IC") || ContainsModel("FX506LU") || ContainsModel("FX506IC") || ContainsModel("FX506LH") || ContainsModel("FA506IV") || ContainsModel("FA706IC") || ContainsModel("FA706IH");
     }
 
 
