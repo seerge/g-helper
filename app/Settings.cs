878a8868666df7719211aa3a6ea9a2b0057ef1f3
naixx <naixx@naixx>
2024-08-30 22:40:15 +0300

    Custom fan curves in tray icon menu

--- app/Settings.cs
+++ app/Settings.cs
@@ -21,6 +21,8 @@ namespace GHelper
     {
         ContextMenuStrip contextMenuStrip = new CustomContextMenu();
         ToolStripMenuItem menuSilent, menuBalanced, menuTurbo, menuEco, menuStandard, menuUltimate, menuOptimized;
+        Dictionary<int, ToolStripMenuItem> menuModesCustom = new ();
+
 
         public GPUModeControl gpuControl;
         public AllyControl allyControl;
@@ -732,6 +734,19 @@ namespace GHelper
             menuTurbo.Checked = (mode == AsusACPI.PerformanceTurbo);
             contextMenuStrip.Items.Add(menuTurbo);
 
+            if (Modes.GetDictionaryCustom().Count > 0)
+                contextMenuStrip.Items.Add("-");
+
+            foreach (var (modeIdx, name) in Modes.GetDictionaryCustom())
+            {
+                var menuCustom = new ToolStripMenuItem(name);
+                menuCustom.Click += (sender, args) => { Program.modeControl.SetPerformanceMode(modeIdx); };
+                menuCustom.Margin = padding;
+                menuCustom.Checked = (modeIdx == Modes.GetCurrent());
+                menuModesCustom.Add(modeIdx, menuCustom);
+                contextMenuStrip.Items.Add(menuCustom);
+            }
+            
             contextMenuStrip.Items.Add("-");
 
             if (isGpuSection)
@@ -1474,6 +1489,10 @@ namespace GHelper
             menuSilent.Checked = false;
             menuBalanced.Checked = false;
             menuTurbo.Checked = false;
+            foreach (var (key, value) in menuModesCustom)
+            {
+                value.Checked = false;
+            }
 
             switch (mode)
             {
@@ -1490,6 +1509,8 @@ namespace GHelper
                     menuBalanced.Checked = true;
                     break;
                 default:
+                    if (menuModesCustom.TryGetValue(mode, out var value))
+                        value.Checked = true;
                     buttonFans.Activated = true;
                     buttonFans.BorderColor = Modes.GetBase(mode) switch
                     {
