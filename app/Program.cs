3dbbddfa5899730f6ed8286c40973d6b0be4e734
Serge <5920850+seerge@users.noreply.github.com>
2024-08-31 10:03:16 +0200

    Startup counter

--- app/Program.cs
+++ app/Program.cs
@@ -69,6 +69,10 @@ namespace GHelper
             Logger.WriteLine("------------");
             Logger.WriteLine("App launched: " + AppConfig.GetModel() + " :" + Assembly.GetExecutingAssembly().GetName().Version.ToString() + CultureInfo.CurrentUICulture + (ProcessHelper.IsUserAdministrator() ? "." : ""));
 
+            var startCount = AppConfig.Get("start_count") + 1;
+            AppConfig.Set("start_count", startCount);
+            Logger.WriteLine("Start Count: " + startCount);
+
             acpi = new AsusACPI();
 
             if (!acpi.IsConnected() && AppConfig.IsASUS())
