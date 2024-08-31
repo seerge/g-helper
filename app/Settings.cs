01cede58b51727774d4b1405fe7163aef61fc7c4
Serge <5920850+seerge@users.noreply.github.com>
2024-08-31 10:42:43 +0200

    UI Tweaks

--- app/Settings.cs
+++ app/Settings.cs
@@ -270,6 +270,12 @@ namespace GHelper
             labelCharge.Click += LabelCharge_Click;
 
             buttonDonate.Click += ButtonDonate_Click;
+            
+            if (AppConfig.Get("start_count") > 10 && !AppConfig.Is("donate_click"))
+            {
+                buttonDonate.BorderColor = colorTurbo;
+                buttonDonate.Badge = true;
+            }
 
             labelDynamicLighting.Click += LabelDynamicLighting_Click;
 
@@ -279,6 +285,8 @@ namespace GHelper
 
         private void ButtonDonate_Click(object? sender, EventArgs e)
         {
+            AppConfig.Set("donate_click", 1);
+            buttonDonate.Badge = false;
             Process.Start(new ProcessStartInfo("https://github.com/seerge/g-helper/wiki/Support-Project") { UseShellExecute = true });
         }
 
--- app/UI/RButton.cs
+++ app/UI/RButton.cs
@@ -52,6 +52,16 @@ namespace GHelper.UI
             }
         }
 
+        private bool badge = false;
+        public bool Badge
+        {
+            get { return badge; }
+            set
+            {
+                badge = value;
+            }
+        }
+
         public RButton()
         {
             DoubleBuffered = true;
@@ -97,6 +107,16 @@ namespace GHelper.UI
                 Region = new Region(pathSurface);
                 pevent.Graphics.DrawPath(penSurface, pathSurface);
                 pevent.Graphics.DrawPath(penBorder, pathBorder);
+            }
+
+            if (badge)
+            {
+                using (Brush brush = new SolidBrush(borderColor))
+                {
+                    var radius = ratio * 10;
+                    var padding = ratio * 10;
+                    pevent.Graphics.FillEllipse(brush, rectSurface.Width - padding - (2 * radius), padding, radius + radius, radius + radius);
+                }
             }
 
             if (!Enabled && ForeColor != SystemColors.ControlText)
