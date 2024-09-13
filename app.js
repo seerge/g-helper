79f8c8edf8e0ba20e2a8cd04957a2799bae82eb7
cheaderthecoder <zeinosman23@gmail.com>
2024-09-13 18:17:59 +0300

    add emoji support

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-// Function to fetch the README content from GitHub
-async function fetchReadme() {
-    const url = 'https://api.github.com/repos/seerge/g-helper/readme';
+document.addEventListener('DOMContentLoaded', () => {
+    // Function to fetch the README content from GitHub
+    async function fetchReadme() {
+        const url = 'https://api.github.com/repos/seerge/g-helper/readme';
 
-    try {
-        const response = await fetch(url, {
-            headers: {
-                'Accept': 'application/vnd.github.v3.raw+json'
+        try {
+            const response = await fetch(url, {
+                headers: {
+                    'Accept': 'application/vnd.github.v3.raw+json'
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch README.');
             }
-        });
 
-        // Throw an error if the response is not okay
-        if (!response.ok) {
-            throw new Error('Failed to fetch README.');
+            const readme = await response.text();
+            console.log('README fetched successfully.');
+
+            const md = window.markdownit().use(window.markdownitEmoji);
+
+            document.getElementById('readme-content').innerHTML = md.render(readme);
+        } catch (error) {
+            console.error('Error:', error);
         }
-
-        // Read the text from the response
-        const readme = await response.text();
-
-        // Inject the README content into the HTML, converting markdown to HTML using marked.js
-        document.getElementById('readme-content').innerHTML = marked.parse(readme);
-    } catch (error) {
-        // Log any errors to the console
-        console.error(error);
     }
-}
 
-// Add an event listener to trigger the fetchReadme function when the page loads
-document.addEventListener('DOMContentLoaded', fetchReadme);
-
+    fetchReadme();
+});
