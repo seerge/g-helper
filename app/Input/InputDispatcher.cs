0434fc1f8df299df9af8fd435d2389991f02c5c4
Serge <5920850+seerge@users.noreply.github.com>
2025-01-24 14:41:41 +0100

    Hardware touchpad toggle for ROG devices https://github.com/seerge/g-helper/issues/3607

--- app/Input/InputDispatcher.cs
+++ app/Input/InputDispatcher.cs
@@ -653,7 +653,14 @@ namespace GHelper.Input
 
         static void ToggleTouchpad()
         {
-            KeyboardHook.KeyKeyKeyPress(Keys.LWin, Keys.LControlKey, Keys.F24, 50);
+            if (AppConfig.IsROG())
+            {
+                AsusHid.WriteInput([AsusHid.INPUT_ID, 0xF4, 0x6B], "USB Touchpad");
+            } else
+            {
+                KeyboardHook.KeyKeyKeyPress(Keys.LWin, Keys.LControlKey, Keys.F24, 50);
+            }
+
         }
 
         static void SleepEvent()
