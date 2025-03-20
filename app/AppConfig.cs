3af87f4979ceb91ca46e3d88c8c03a2d69720bd6
Serge <5920850+seerge@users.noreply.github.com>
2025-03-20 09:30:27 +0100

    4ZoneRGB support for G614JU https://github.com/seerge/g-helper/issues/3870

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -496,7 +496,7 @@ public static class AppConfig
 
     public static bool IsPossible4ZoneRGB()
     {
-        return ContainsModel("G614JI_") || ContainsModel("G614JV_") || ContainsModel("G614JZ") || IsStrixLimitedRGB();
+        return ContainsModel("G614JI_") || ContainsModel("G614JV_") || ContainsModel("G614JZ") || ContainsModel("G614JU") || IsStrixLimitedRGB();
     }
 
     public static bool Is4ZoneRGB()
