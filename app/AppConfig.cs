2d19b59a2ededdab1b11ad221adb65406d16d156
Serge <5920850+seerge@users.noreply.github.com>
2025-11-27 14:29:37 +0100

    Force Mini-led setting for G635LW https://github.com/seerge/g-helper/issues/4706

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -728,7 +728,7 @@ public static class AppConfig
 
     public static bool IsForceMiniled()
     {
-        return ContainsModel("G834JYR") || ContainsModel("G834JZR") || ContainsModel("G634JZR") || ContainsModel("G835LW") || Is("force_miniled");
+        return ContainsModel("G834JYR") || ContainsModel("G834JZR") || ContainsModel("G634JZR") || ContainsModel("G835LW") || ContainsModel("G635LW") || Is("force_miniled");
     }
     public static bool SaveDimming()
     {
