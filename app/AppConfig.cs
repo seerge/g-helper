ea51ef9a6678cc56a37af4824092ba693b33af19
Serge <5920850+seerge@users.noreply.github.com>
2026-04-11 21:25:10 +0200

    Mode reset tweak only for G14 2024

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -563,7 +563,7 @@ public static class AppConfig
 
     public static bool IsResetRequired()
     {
-        return ContainsModel("GA403") || ContainsModel("FA507XV");
+        return ContainsModel("GA403UI") || ContainsModel("GA403UU") || ContainsModel("GA403UV") || ContainsModel("FA507XV");
     }
 
     public static bool IsFanRequired()
