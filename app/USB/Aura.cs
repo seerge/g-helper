447c60eddc1632a57168fd343a54bf94630aaf16
Serge <5920850+seerge@users.noreply.github.com>
2026-06-08 12:12:43 +0200

    Second color for Breath/Color Cycle https://github.com/seerge/g-helper/issues/5530

--- app/USB/Aura.cs
+++ app/USB/Aura.cs
@@ -252,7 +252,7 @@ namespace GHelper.USB
 
         public static bool HasSecondColor()
         {
-            return (mode == AuraMode.AuraBreathe || mode == AuraMode.GRADIENT) && !isACPI;
+            return (mode == AuraMode.AuraBreathe || mode == AuraMode.GRADIENT) && (!isACPI || AppConfig.IsDynamicLightingOnly());
         }
 
         private static void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
