2b3acf89713ea99701f8b3fbc7b42038747a455c
Serge <5920850+seerge@users.noreply.github.com>
2026-07-08 13:50:20 +0200

    Cleanup and simplifications

--- app/Input/InputDispatcher.cs
+++ app/Input/InputDispatcher.cs
@@ -1325,10 +1325,7 @@ namespace GHelper.Input
             if (string.IsNullOrEmpty(command)) return;
             try
             {
-                if (command.StartsWith("shutdown"))
-                    ProcessHelper.RunCMD("cmd", "/C " + command);
-                else
-                    RestrictedProcessHelper.RunAsRestrictedUser(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "cmd.exe"), "/C " + command);
+                RestrictedProcessHelper.RunAsRestrictedUser(command);
             }
             catch (Exception ex)
             {
