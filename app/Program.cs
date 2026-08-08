88a29808750470884aa6a97a27b220f6656c748f
Serge <5920850+seerge@users.noreply.github.com>
2026-08-08 14:02:52 +0200

    WMI fallback for a startup task

--- app/Program.cs
+++ app/Program.cs
@@ -505,11 +505,11 @@ namespace GHelper
                 if (limit > 0 && limit < 100)
                 {
                     Logger.WriteLine($"------- Startup Battery Limit {limit} -------");
-                    ProcessHelper.StartEnableService("ATKWMIACPIIO", false);
                     Logger.WriteLine($"Connecting to ACPI");
                     acpi = new AsusACPI();
                     Logger.WriteLine($"Setting Limit");
-                    acpi.DeviceSet(AsusACPI.BatteryLimit, limit, "Limit");
+                    if (acpi.IsConnected()) acpi.DeviceSet(AsusACPI.BatteryLimit, limit, "Limit");
+                    else AsusACPI.DeviceSetWmi(AsusACPI.BatteryLimit, limit);
                 }
             }
             catch (Exception ex)
