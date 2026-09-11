dac255c94d20069fcf3e40daeca730b1492d34e7
Serge <5920850+seerge@users.noreply.github.com>
2026-09-11 10:41:32 +0200

    Added FX608 to the eco-boot fix list https://github.com/seerge/g-helper/issues/6012

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -509,7 +509,7 @@ public static class AppConfig
 
     public static bool IsEcoBootFix()
     {
-        return ContainsModel("G635L") || ContainsModel("G615L") || ContainsModel("G835L") || ContainsModel("G815L") || ContainsModel("FA506");
+        return ContainsModel("G635L") || ContainsModel("G615L") || ContainsModel("G835L") || ContainsModel("G815L") || ContainsModel("FA506") || ContainsModel("FX608");
     }
 
     public static bool IsELMB()
